7da54fe7aa94d7b28f95d26c65f284b3b26e1d25
Terence Faid JABO <faidterence@outlook.com>
2024-05-09 21:09:34 +0200

    Divide and conquer Quick sort

---
+++ DivideAndConqerQuickSort.py
@@ -0,0 +1,26 @@
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+# Example usage:
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quick_sort(arr, 0, n - 1)
+print(arr)  # Output: [1, 5, 7, 8, 9, 10]
